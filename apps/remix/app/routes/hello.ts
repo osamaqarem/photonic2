2e5435bb63e75ee4a1d07e05e139cd2018c68681
Osama Qarem <osamaqarem@gmail.com>
2023-07-02 02:12:01 +0200

    Docker file and flyio deployment

--- apps/remix/app/routes/hello.ts
+++ apps/remix/app/routes/hello.ts
@@ -4,5 +4,5 @@ import { json } from "@remix-run/node"
 export async function loader({ request }: LoaderArgs) {
   const url = new URL(request.url)
   const name = url.searchParams.get("name") || "World"
-  return json({ message: `Hello, ${name}!` })
+  return json({ message: `Hello, ${name}!`, env: process.env.NODE_ENV })
 }
