1a26f66f517f4370519bda57c49eb6da3b44b7ef
Osama Qarem <osamaqarem@gmail.com>
2023-09-21 22:09:04 +0200

    Remove /apps/remix

--- apps/remix/app/routes/hello.ts
+++
@@ -1,8 +0,0 @@
-import type { LoaderArgs } from "@remix-run/node"
-import { json } from "@remix-run/node"
-
-export async function loader({ request }: LoaderArgs) {
-  const url = new URL(request.url)
-  const name = url.searchParams.get("name") || "World"
-  return json({ message: `Hello, ${name}!`, env: process.env.NODE_ENV })
-}
