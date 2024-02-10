aaa7ed804222cf56f22a6b2616bb09503b02ad6f
Osama Qarem <osamaqarem@gmail.com>
2024-02-10 23:10:48 +0100

    Implement incremental changes handling

--- apps/expo/src/features/home/hooks/useAssets.ts
+++ apps/expo/src/features/home/hooks/useAssets.ts
@@ -1,7 +1,12 @@
 import { Logger } from "@photonic/common"
+import { eq } from "drizzle-orm"
 import * as ExpoMedia from "expo-media-library"
 import React from "react"
-import type { GenericAsset } from "~/expo/features/home/utils/media-manager"
+import { useAlerts } from "~/expo/design/components/alerts/useAlerts"
+import type {
+  GenericAsset,
+  LocalMediaAsset,
+} from "~/expo/features/home/utils/media-manager"
 import { exportPhotoSchemaObject } from "~/expo/features/home/utils/media-manager"
 import { db } from "~/expo/lib/db"
 import { assets } from "~/expo/lib/db/schema"
@@ -17,35 +22,76 @@ const databasePopulatedStorage = {
 
 export const useAssets = () => {
   const [photoDbState, setPhotoDbState] = React.useState<
-    "building" | "permissionNeeded" | "ready"
+    "building" | "permissionNeeded" | "permissionGranted" | "ready"
   >()
   const [data, setData] = React.useState<Array<GenericAsset>>()
+  const { showNotification } = useAlerts()
 
   React.useEffect(() => {
     const init = async () => {
-      const fetchAssets = async () => {
-        const data = await db.select().from(assets)
-        setPhotoDbState("ready")
-        setData(data as Array<GenericAsset>)
-        logger.log("assets length: " + data.length)
-      }
-
-      if (!photoDbState || photoDbState === "permissionNeeded") {
+      if (!photoDbState) {
         const res = await ExpoMedia.getPermissionsAsync()
         if (res.granted) {
-          const databasePopulated = databasePopulatedStorage.get()
-          if (!databasePopulated) {
-            setPhotoDbState("building")
-            populateDB()
-          }
-          fetchAssets()
+          setPhotoDbState("permissionGranted")
         } else {
           setPhotoDbState("permissionNeeded")
         }
+      } else if (photoDbState === "permissionGranted") {
+        const databasePopulated = databasePopulatedStorage.get()
+        if (!databasePopulated) {
+          setPhotoDbState("building")
+          populateDB()
+        }
+        const data = await db.select().from(assets)
+        setData(data as Array<GenericAsset>)
+        setPhotoDbState("ready")
       }
     }
     init()
   }, [photoDbState])
+
+  React.useEffect(() => {
+    const updateAsset = async (asset: LocalMediaAsset) => {
+      if (asset.mediaType !== "photo") return
+      await db
+        .update(assets)
+        .set({
+          creationTime: new Date(asset.creationTime),
+          duration: asset.duration,
+          height: asset.height,
+          width: asset.width,
+          localId: asset.id,
+          name: asset.filename,
+          uri: asset.uri,
+        })
+        .where(eq(assets.name, asset.filename))
+    }
+
+    const sub = ExpoMedia.addListener(async change => {
+      if (change.hasIncrementalChanges) {
+        for (const updated of change.updatedAssets ?? []) {
+          await updateAsset(updated)
+        }
+        for (const item of change.deletedAssets ?? []) {
+          await db.delete(assets).where(eq(assets.name, item.filename))
+        }
+        if (change.insertedAssets) {
+          await db
+            .insert(assets)
+            .values(change.insertedAssets.map(exportPhotoSchemaObject))
+        }
+      } else {
+        // TODO: rebuild DB of local assets
+        showNotification({
+          message: "Received non-incremental changes.",
+          dismissAfterMs: 5000,
+        })
+      }
+    })
+    return () => {
+      sub.remove()
+    }
+  }, [showNotification])
 
   return { assets: data, photoDbState }
 }
