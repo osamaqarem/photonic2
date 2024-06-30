1cb3f12a06abd8d842c416272216ed327a537ac5
Osama Qarem <osamaqarem@gmail.com>
2024-06-30 19:07:22 +0200

    Add sentry to nextjs
    closes PHO-25

---
+++ apps/next/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://62bc1878211c4740ac68cbc7c04d46b5@o1418518.ingest.us.sentry.io/6761726",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
