84b86479df0d155221e68415133248fd3aa478bd
Osama Qarem <osamaqarem@gmail.com>
2024-07-06 09:37:47 +0200

    Add unique constraints to assets table (#1)
    The issue was not recycling. It was duplicate rows being inserted into
    the assets table.

---
+++ apps/expo/src/db/migrations/0002_friendly_talos.sql
@@ -0,0 +1,3 @@
+CREATE UNIQUE INDEX `asset_localId_unique` ON `asset` (`localId`);--> statement-breakpoint
+CREATE UNIQUE INDEX `asset_name_unique` ON `asset` (`name`);--> statement-breakpoint
+CREATE UNIQUE INDEX `asset_string_unique` ON `asset` (`string`);
